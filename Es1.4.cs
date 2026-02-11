8ac36064e44553ec7664175625cd582d0b594daf
SgaggeroAlessandro <14529841@itisrossi.vi.it>
2026-02-11 17:52:42 +0100

    Aggiunta dei file

---
+++ Es1.4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Es1array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num;
+            int n;
+            bool diversi = true;
+
+            do
+            {
+                Console.WriteLine("Scrivi quanti numeri vuoi inserire");
+            } while (!int.TryParse(Console.ReadLine(), out n) || n <= 0);
+            num = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Inserisci un numero");
+                int.TryParse(Console.ReadLine(), out num[i]);
+            }
+            for (int i = 0; i < n && diversi; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (num[i] == num[j])
+                    {
+                        diversi = false;
+                        break;
+                    }
+                }
+            }
+
+            if (diversi == true)
+            {
+                Console.WriteLine("Risultato finale : 1");
+            }
+            else
+            {
+                Console.WriteLine("Risultato finale: 0");
+            }
+        }
+    }
+}
---
+++ Es2.4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Es1array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[30];
+
+            Random r = new Random();
+            
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = r.Next(0, 101);
+                Console.Write(v[i] + " ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Coppie trovate:");
+            for (int i = 0; i < 30; i++)
+            {
+                for (int j = 0; j < 30; j++)
+                {
+                    if (i != j && v[i] == 2 * v[j])
+                    {
+                        Console.WriteLine(v[i] + " " + v[j]);
+                    }
+
+                }
+            }
+        }
+    }
+}
